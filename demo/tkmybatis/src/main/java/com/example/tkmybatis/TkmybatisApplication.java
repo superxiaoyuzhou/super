8dfd9d6116301a9f8782ebbe1fcc01e8466b82b2
Piter <1181720735@qq.com>
2019-02-14 15:52:34 +0800

    提交

--- demo/tkmybatis/src/main/java/com/example/tkmybatis/TkmybatisApplication.java
+++ demo/tkmybatis/src/main/java/com/example/tkmybatis/TkmybatisApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * tkmybatis
+ */
 @SpringBootApplication
 @MapperScan("com.example.tkmybatis.mapper")
 public class TkmybatisApplication {
