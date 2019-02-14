c23beed7b4aa317979572b3acc7e0f56c209a7b0
Piter <1181720735@qq.com>
2019-02-14 15:50:19 +0800

    提交

--- demo/mapper/src/main/java/com/example/mapper/MapperApplication.java
+++ demo/mapper/src/main/java/com/example/mapper/MapperApplication.java
@@ -3,6 +3,9 @@ package com.example.mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * mapper生成插件
+ */
 @SpringBootApplication
 public class MapperApplication {
 
